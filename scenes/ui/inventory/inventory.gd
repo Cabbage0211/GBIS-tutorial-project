5788ae3670e7b6ee34f3f52d495bfe2b413cba4b
Cabbage0211 <yang.leo.0211@gmail.com>
2025-06-20 10:04:49 +0800

    更新插件

--- scenes/ui/inventory/inventory.gd
+++ scenes/ui/inventory/inventory.gd
@@ -2,6 +2,6 @@ extends ColorRect
 class_name Inventory
 
 func _ready() -> void:
-	hide()
 	mouse_entered.connect(func(): Global.player.is_busy = true)
 	mouse_exited.connect(func(): Global.player.is_busy = false)
+	hide()
